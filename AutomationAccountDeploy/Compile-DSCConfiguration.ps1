f8d36da0f0e68dab854bbe32c4fda5307bd00bb2
Gordon Byers <gordonbyers@yahoo.com>
2017-11-26 22:31:23 +0000

    Scripts to domain join

---
+++ AutomationAccountDeploy/Compile-DSCConfiguration.ps1
@@ -0,0 +1,23 @@
+﻿#This initiates a compilation of a DSC Configuration.
+#The portal doesn't allow these params to be set
+#So we have to do it by PowerShell.
+
+param(
+    $AutomationAccountName="dscauto",
+    $AutomationAccountResourceGroup="DSCPlay",
+    $ConfigurationName = "GDomain"
+)
+
+Login-AzureRmAccount
+
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName = "SimpleDomJoin" 
+            PSDscAllowPlainTextPassword = $True
+            PSDscAllowDomainUser = $True
+        }
+    )
+}
+
+Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $AutomationAccountResourceGroup -AutomationAccountName $AutomationAccountName -ConfigurationName $ConfigurationName -ConfigurationData $ConfigData
